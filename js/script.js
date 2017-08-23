56e525a47a7fbfeaef64ffb6def72a9a2b6d3ee1
Dok1211 <wataru.douke@gmail.com>
2017-08-23 16:45:58 +0900

    sp view finished

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function panelOpen(){
+	if(document.getElementById("disp").style.display == "grid"){
+		document.getElementById("disp").style.display = "none";
+	}else{
+		document.getElementById("disp").style.display ="grid";
+	}
+}
